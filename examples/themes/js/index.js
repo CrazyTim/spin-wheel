7ea3a5ca5640ccb02eb2c6a8024e9ff2b5d0ba50
CrazyTim <impdeveloper@gmail.com>
2024-06-21 21:44:25 +1000

    Add spin button to themes example

--- examples/themes/js/index.js
+++ examples/themes/js/index.js
@@ -3,14 +3,10 @@ import {loadFonts} from '../../../scripts/util.js';
 import {props} from './props.js';
 
 window.onload = async () => {
-  await loadFonts(props.map(i => i.itemLabelFont));
-  init();
-};
 
-function init() {
+  await loadFonts(props.map(i => i.itemLabelFont));
 
   const wheel = new Wheel(document.querySelector('.wheel-wrapper'));
-
   const dropdown = document.querySelector('select');
 
   // Initalise dropdown with the names of each example:
@@ -35,4 +31,30 @@ function init() {
   // Save object globally for easy debugging.
   window.wheel = wheel;
 
-}
+  const btnSpin = document.querySelector('button');
+  let modifier = 0;
+
+  window.addEventListener('click', (e) => {
+
+    // Listen for click event on spin button:
+    if (e.target === btnSpin) {
+      const {duration, winningItemRotaion} = calcSpinToValues();
+      wheel.spinTo(winningItemRotaion, duration);
+    }
+
+  });
+
+  function calcSpinToValues() {
+    const duration = 3000;
+    const winningItemRotaion = getRandomInt(360, 360 * 1.75) + modifier;
+    modifier += 360 * 1.75;
+    return {duration, winningItemRotaion};
+  }
+
+  function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min)) + min;
+  }
+
+};
